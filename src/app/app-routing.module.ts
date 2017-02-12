21f34a45deb39636327f7543f25e29920e8426ca
Will保哥 <doggy.huang@gmail.com>
2017-02-12 16:30:10 +0800

    14 建立含路由的 charts 模組，並將所有相依元件與路由全部移往 ChartsModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,23 +4,12 @@ import { Route, Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 
-import { FlotComponent } from './flot/flot.component';
-import { RadialComponent } from './radial/radial.component';
-import { RickshawComponent } from './rickshaw/rickshaw.component';
-
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: 'dashboard',   component: DashboardComponent },
   { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts/:username',
-    children: [
-      { path: '',         redirectTo: 'flot', pathMatch: 'full' },
-      { path: 'flot',     component: FlotComponent },
-      { path: 'radial',   component: RadialComponent },
-      { path: 'rickshaw', component: RickshawComponent }
-    ]
-  },
+
   fallbackRoute
 ];
 
