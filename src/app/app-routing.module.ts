e45b00d684a0d2d3920f988bb0767ed6fdf7943a
Will 保哥 <doggy.huang@gmail.com>
2017-02-12 11:46:58 +0800

    移除所有練習用的元件，準備開始套版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Route, Routes, RouterModule } from '@angular/router';
 
-import { LayoutComponent } from './layout/layout.component';
-import { Page2Component } from './page2/page2.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
-  { path: '',         redirectTo: '/layout',  pathMatch: 'full' },
-  { path: 'layout',   component: LayoutComponent },
-  { path: 'page2',    component: Page2Component },
+  { path: 'dashboard',    component: DashboardComponent },
   fallbackRoute
 ];
 
