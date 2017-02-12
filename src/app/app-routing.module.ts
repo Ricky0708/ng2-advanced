1ab337cdf9be06181054bf39f573a4e2bf0404fb
Will 保哥 <doggy.huang@gmail.com>
2017-02-12 13:51:21 +0800

    在 charts 子路由設定一組預設路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,7 @@ const routes: Routes = [
   { path: 'cards',     component: CardsComponent },
   { path: 'charts',
     children: [
+      { path: '',         redirectTo: 'flot', pathMatch: 'full' },
       { path: 'flot',     component: FlotComponent },
       { path: 'radial',   component: RadialComponent },
       { path: 'rickshaw', component: RickshawComponent }
