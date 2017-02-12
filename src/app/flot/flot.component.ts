307b078285573a099ada201f7a42d08bf8d5542d
Will保哥 <doggy.huang@gmail.com>
2017-02-12 15:33:57 +0800

    12 練習將上層路由的路由參數 Observable 取消訂閱

--- src/app/flot/flot.component.ts
+++ src/app/flot/flot.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { OnDestroy, Component,  OnInit} from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
+
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-flot',
   templateUrl: './flot.component.html',
   styleUrls: ['./flot.component.css']
 })
-export class FlotComponent implements OnInit {
+export class FlotComponent implements OnInit, OnDestroy {
 
   username: string;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
+  parentParams$: Subscription;
+
   ngOnInit() {
-    this.username = this.route.parent.snapshot.params['username'];
+    this.parentParams$ = this.route.parent.params.subscribe(params => {
+      console.log(params['username']);
+    });
   }
 
+  ngOnDestroy() {
+    this.parentParams$.unsubscribe();
+  }
 }
