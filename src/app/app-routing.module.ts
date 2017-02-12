f23f3afe086f66710f6f4a977ae0cb98168092d3
Will 保哥 <doggy.huang@gmail.com>
2017-02-12 09:57:29 +0800

    01 實作第一層路由的 LayoutComponent 顯示 02 實作第一層路由的 Page2Component 顯示 ( 在瀏覽器網址列手動切換路由 )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LayoutComponent } from './layout/layout.component';
+import { Page2Component } from './page2/page2.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '',         component: LayoutComponent },
+  { path: 'page2',    component: Page2Component }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
   providers: []
 })
