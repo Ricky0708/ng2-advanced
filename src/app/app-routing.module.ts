e04a08a07689f41adc81359cf860d7afd5893798
Will 保哥 <doggy.huang@gmail.com>
2017-02-12 13:41:44 +0800

    08 加入 Charts 子路由與左側選單套版

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,22 @@ import { Route, Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
 
+import { FlotComponent } from './flot/flot.component';
+import { RadialComponent } from './radial/radial.component';
+import { RickshawComponent } from './rickshaw/rickshaw.component';
+
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards',     component: CardsComponent },
+  { path: 'charts',
+    children: [
+      { path: 'flot',     component: FlotComponent },
+      { path: 'radial',   component: RadialComponent },
+      { path: 'rickshaw', component: RickshawComponent }
+    ]
+  },
   fallbackRoute
 ];
 
