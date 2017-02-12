832168704223cdd466f83d01d2079d5c74fb555a
Will 保哥 <doggy.huang@gmail.com>
2017-02-12 11:57:01 +0800

    07 完成 Centric 套版 (完成 dashboard 與 cards 頁面套版)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Route, Routes, RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 import { fallbackRoute } from './shared/fallback-route';
 
 const routes: Routes = [
-  { path: 'dashboard',    component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cards',     component: CardsComponent },
   fallbackRoute
 ];
 
