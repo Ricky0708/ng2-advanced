732563978c6733b74a5f4760eb0344bd219a7248
Will保哥 <doggy.huang@gmail.com>
2017-02-19 09:15:34 +0800

    示範用正確的方法整合第三方 JavaScript 函式庫進來

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {NgZone, Component,  OnInit} from '@angular/core';
 
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -11,10 +11,17 @@ import { initDashboard } from '../shared/app-init';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  constructor(private router: Router, private route: ActivatedRoute,
+              private zone: NgZone) { }
 
   ngOnInit() {
-    initDashboard();
+    this.zone.runOutsideAngular(() => {
+      initDashboard();
+    });
+  }
+
+  debug() {
+    console.log('Dashboard', 'debug()');
   }
 
   goCards(type) {
