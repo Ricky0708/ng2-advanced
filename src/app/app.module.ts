513b5e4d54352d6c344c4dcf08aca0d036a1c0cf
Will保哥 <doggy.huang@gmail.com>
2017-02-19 09:55:02 +0800

    17 練習 NgForm 與 NgModel 的基本應用技巧

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -19,6 +19,7 @@ import { FormComponent } from './form/form.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     AppRoutingModule
   ],
