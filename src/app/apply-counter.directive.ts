17664355824146c9224ff005fd890f31dc9c0631
Will保哥 <doggy.huang@gmail.com>
2017-02-19 16:04:25 +0800

    示範 ElementRef 與 Renderer 的用法

--- src/app/apply-counter.directive.ts
+++ src/app/apply-counter.directive.ts
@@ -1,4 +1,4 @@
-import { HostListener, HostBinding, Directive, Input } from '@angular/core';
+import {Renderer, HostListener,  HostBinding,  Directive,  Input,  ElementRef} from '@angular/core';
 
 @Directive({
   selector: '.pt0[apply-counter]'
@@ -15,12 +15,17 @@ export class ApplyCounterDirective {
     console.log($event);
   }
 
-  constructor() {
+  constructor(private el: ElementRef, private renderer: Renderer) {
 
     // setTimeout(() => {
     //   this.bgClass = true;
     // }, 5000);
 
+  }
+
+  ngOnInit() {
+    (this.el.nativeElement as HTMLDivElement).innerHTML = '123';
+    this.renderer.setElementClass(this.el.nativeElement, 'bg-danger', true);
   }
 
 }
